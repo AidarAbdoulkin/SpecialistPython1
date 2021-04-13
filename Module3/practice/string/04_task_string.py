57f5b6ee634f0e68a09056c18f82989139e819e3
AidarAbdoulkin <Aidar.Abdulkin@yandex.ru>
2021-04-13 13:03:18 +0300

    Update 04_task_string.py

--- Module3/practice/string/04_task_string.py
+++ Module3/practice/string/04_task_string.py
@@ -3,5 +3,8 @@
 
 text = "Lorem ipsum dolor sit amet consectetur adipiscing elit Integer porttitor bibendum nisi ut convallis ante"
 # Примечание: для генериации текста можете воспользоваться сайтом: https://ru.lipsum.com/
-
-# TODO: your code here
+i = 0
+for t in text.split(" "):
+    if len(t) > 5:
+        i += 1
+print(i)
