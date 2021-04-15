efe382f7575c3546de37c9f23d032f87f54af787
AidarAbdoulkin <Aidar.Abdulkin@yandex.ru>
2021-04-15 11:58:46 +0300

    Update 02_task_file.py

--- Module6/practice/files/02_task_file.py
+++ Module6/practice/files/02_task_file.py
@@ -4,3 +4,16 @@
 # 1. На какую сумму было продано товаров
 # 2. Цену самого дорогого товара
 # 3. Цену самого дешевого товара
+
+f = open("data/sold.txt", "r")
+chks = []
+for line in f:
+    chks += line.rstrip().split(" ")
+f.close()
+
+for index, item in enumerate(chks):
+    chks[index] = float(item)
+
+print("sum:", sum(chks))
+print("max:", max(chks))
+print("min", min(chks))
